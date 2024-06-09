7accf278f0d48fd35b6cf23d8be0b4ed515c10f0
sain0136 <55252053+sain0136@users.noreply.github.com>
2024-06-09 15:53:12 -0400

    chore: Update dependencies for rand and colored crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use  colored::*;
+
 fn main() {
-    println!("This is the main function in src/main.rs");
+    println!("This is a guessing game");
+    // mut allows this variable to be changed
+    loop {
+        println!("Please input your guess.");
+        let mut guess: String = String::new();
+        let secret_number = rand::thread_rng().gen_range(1, 5);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("The secrert number is {}", secret_number);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("{}","You win".green());
+                break;
+            }
+            Ordering::Less => println!("{}","Too small".red()),
+            Ordering::Greater => println!("{}","Too big".red()),
+        }
+    }
 }
