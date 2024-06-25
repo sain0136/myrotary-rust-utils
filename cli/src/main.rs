f0ca976e68055239f85b3df8e51b43bbd1e89f8c
sain0136 <55252053+sain0136@users.noreply.github.com>
2024-06-25 17:56:57 -0400

    chore: Remove unnecessary files and dependencies and restructure project.

---
+++ cli/src/main.rs
@@ -0,0 +1,34 @@
+mod commands;
+use clap::Parser;
+
+const VERSION: &str = "1.0.0";
+const ABOUT: &str = "My Rotary Server Rust CLI";
+const LONG_ABOUT: &str = "My Rotary Server Rust CLI -- An user-friendly CLI for server administration and file management.";
+// use commands::game;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version = VERSION, about = ABOUT, long_about = LONG_ABOUT)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long, default_value_t = String::from("Nameless"))]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+
+    /// Play a game
+    #[arg(short, long)]
+    game: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.game {
+        commands::game::guessing_game("Sebastien");
+    } else {
+        println!("Hello,{} you called this program {} times!", args.name, args.count);
+    }
+}
---
+++ server/target/debug/deps/server-8f5a8583a774e283.d
@@ -0,0 +1,5 @@
+C:\Users\Sebastien Saint Rose\Documents\Github\myrotary-rust-utils\server\target\debug\deps\libserver-8f5a8583a774e283.rmeta: src\main.rs
+
+C:\Users\Sebastien Saint Rose\Documents\Github\myrotary-rust-utils\server\target\debug\deps\server-8f5a8583a774e283.d: src\main.rs
+
+src\main.rs:
