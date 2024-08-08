616972780bdd1e086868798f095cc34f87cdcfa5
sain0136 <55252053+sain0136@users.noreply.github.com>
2024-08-08 13:43:05 -0400

    chore: Add println statement for testing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -43,7 +43,7 @@ fn main() {
     clear_screen();
     let args = Args::parse();
     prod();
-
+    println!("Testing");
     match args {
         Args { game: true, .. } => commands::game::guessing_game("Sebastien"),
         Args {
